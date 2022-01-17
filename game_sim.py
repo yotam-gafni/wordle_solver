742c0c022690723e1f39c3132bfecb5501d3d826
Shachar Raindel <shacharr@microsoft.com>
2022-01-17 15:21:04 -0800

    Switch from string processing to word indices, to improve efficiency
    Before:
    wordle_solver$ time pypy3 game_sim.py
    ...
    real    2m30.695s
    user    2m27.403s
    sys     0m3.250s
    
    After:
    wordle_solver$ time pypy3 game_sim.py
    ...
    real    1m27.037s
    user    1m24.965s
    sys     0m2.030s

--- game_sim.py
+++ game_sim.py
@@ -2,12 +2,19 @@ from functools import lru_cache
 
 counter_example_words = set(["bills","cills","dills","fills","gills","hills", "jills", "kills","lills","mills","nills","pills","rills","sills","tills","vills","wills","yills","zills"])
 
+def msum_to_int(msum):
+    total = 0
+    for i in msum:
+        total = total*3 + i
+    return total
 
 @lru_cache(maxsize=None)
-def calc_response_vector(target,guess):
-    w1 = guess.strip()
-    w2 = target.strip()
-    tw2 = w2
+def calc_response_vector(w1,w2):
+    #print(w1)
+    #print(w2)
+    w1 = all_lines_words[w1]
+    w2 = all_lines_words[w2]
+    tw2 = str(w2)
     msum = [0 for i in range(5)]
     for c_ind in range(5):
         if w1[c_ind] == tw2[c_ind]:
@@ -18,13 +25,36 @@ def calc_response_vector(target,guess):
             msum[c_ind] = 1
             ind_app = tw2.find(w1[c_ind])
             tw2 = tw2[:ind_app] + "*" + tw2[ind_app+1:]
-    return msum
+    return msum_to_int(tuple(msum))
+
+def calc_response_vector_no_int(w1,w2):
+    #print(w1)
+    #print(w2)
+    w1 = all_lines_words[w1]
+    w2 = all_lines_words[w2]
+    tw2 = str(w2)
+    msum = [0 for i in range(5)]
+    for c_ind in range(5):
+        if w1[c_ind] == tw2[c_ind]:
+            msum[c_ind] = 2
+            tw2 = tw2[:c_ind] + "*" + tw2[c_ind+1:]
+    for c_ind in range(5):
+        if w1[c_ind] in tw2 and msum[c_ind] == 0:
+            msum[c_ind] = 1
+            ind_app = tw2.find(w1[c_ind])
+            tw2 = tw2[:ind_app] + "*" + tw2[ind_app+1:]
+    return tuple(msum)
+
+all_w = open("words.txt","r")
+all_lines_words = [x.strip() for x in all_w.readlines()]
+snare_idx = all_lines_words.index("snare")
+
+
 
 def run_game(target):
-    f = open("words.txt","r")
-    all_w = open("words.txt","r")
-    lines = f.readlines()
-    all_lines = all_w.readlines()
+    all_lines = range(len(all_lines_words))
+    lines = range(len(all_lines_words))
+    target_idx = all_lines_words.index(target)
     for round in range(10):
         min_wc = 100000
         chosen_word = ""
@@ -32,42 +62,41 @@ def run_game(target):
         if round != 0:
             all_it = all_lines
         else:
-            all_it = ["snare"]
+            all_it = [snare_idx]
         for w1 in all_it:
-            w1 = w1.strip()
             rmat = {}
             for w2 in lines:
-                w2 = w2.strip()
-                tw2 = w2
-                msum = calc_response_vector(w2,w1)
-                if tuple(msum) not in rmat:
-                    rmat[tuple(msum)] = [w2]
+                msum = calc_response_vector(w1,w2)
+                if msum not in rmat:
+                    rmat[msum] = [w2]
                 else:
-                    rmat[tuple(msum)].append(w2)
+                    rmat[msum].append(w2)
             M = max([len(val) for val in rmat.values()])
             if M < min_wc:
                 min_wc = M
                 chosen_word = w1
                 srmat = rmat
-        print(chosen_word)
+        print(all_lines_words[chosen_word])
         #inp = input()
         #print(inp)
-        inp = calc_response_vector(target,chosen_word)
+        inp = calc_response_vector_no_int(chosen_word,target_idx)
         print(inp)
-        feedback = tuple([int(el) for el in inp])
+        feedback = msum_to_int(tuple([int(el) for el in inp]))
+        if feedback not in srmat:
+            print("Unexpected feedback {}, srmat: {}".format(feedback,srmat))
         lines = srmat[feedback]
         if len(lines) == 1:
-            print("Done. Final word is {}".format(lines[0]))
+            print("Done. Final word is {}".format(all_lines_words[lines[0]]))
             return True
         if round > 4:
-            print("Beyond limit, option set:{}".format(lines))
+            print("Beyond limit, option set:{}".format([all_lines_words[x] for x in lines]))
     print("Failed. Did not find word after 6 attempts")
     return False
 
 
 def main():
-    print(calc_response_vector("fills","snare"))
-    print(calc_response_vector("fills","moult"))
+    #print(calc_response_vector("fills","snare"))
+    #print(calc_response_vector("fills","moult"))
     for w in counter_example_words:
         print("Playing for {}:".format(w))
         if not run_game(w):
